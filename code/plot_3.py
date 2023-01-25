e3ec73e075faeaa6fb66f336b15096896ba3a3dc
Mandzhi <mila.mana@yandex.ru>
2023-01-25 15:25:45 +0300

    Add files via upload

---
+++ code/plot_3.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+## Bar chart horizontal "Top-8 stadiums on capacity" ##
+labels = ['Estádio do Maracanã', 'Camp Nou', 'Estadio Azteca',
+          'Wembley Stadium', 'Rose Bowl', 'Estadio Santiago Bernabéu',
+          'Estadio Centenario', 'Lusail Stadium']
+capacity = [200, 121, 115, 99, 94, 90, 90, 89]
+plt.barh(np.arange(len(capacity)), capacity)
+
+# make labels for each bar
+for count, cap_data in enumerate(capacity):
+    plt.text(x=cap_data+1,
+             y=count,
+             s=f"{cap_data}", color='black',
+             va='center', fontweight='bold')
+plt.title('Top-8 stadiums on capacity (in thousands)')
+plt.yticks(np.arange(len(capacity)), labels = labels, fontsize = 10)
+
+# get rid of the frame
+for spine in plt.gca().spines.values():
+    spine.set_visible(False)
+
+# remove x-label ticks
+plt.xticks([])
+
+plt.savefig('figure_3.jpeg', dpi=600, bbox_inches='tight')   
+plt.show()
